88323db1e8b555ee52a0adcb2fb55925bb15531f
NANDHAGOPAL-ASPIRE <nandhagopal.ramesh@aspiresys.com>
2018-07-16 11:33:56 +0530

    Samples

---
+++ NANDHAGOPAL/Program1/.metadata/.plugins/org.eclipse.core.resources/.history/47/b088dca92c84001811d587f138f878ed
@@ -0,0 +1,17 @@
+
+public class Student {
+	
+	private String name;
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void print()
+	{
+		System.out.println(name);
+	}
+	
+
+}
---
+++ NANDHAGOPAL/Program1/sampleproject/src/main/java/com/aspire/sample/Student.java
@@ -0,0 +1,21 @@
+package com.aspire.sample;
+
+public class Student {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void print()
+	{
+		System.out.println(name+"........");
+	}
+	
+
+}
